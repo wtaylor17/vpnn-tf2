f3bff143a687dbbfc11f45af2bba6276a8cdf6cf
logan-anderson <landerson2@upei.ca>
2020-10-20 20:27:54 -0300

    added keras load data

--- test.py
+++ test.py
@@ -1,41 +1,50 @@
+from tensorflow.python.keras.engine.input_layer import Input
+from vpnn import layers
+from tensorflow import keras
 from vpnn.models import vpnn
 import tensorflow as tf
-from tensorflow.python.ops.gen_math_ops import mod
-import tensorflow_datasets as tfds
-from vpnn import VpnnLayer
+# from tensorflow.python.ops.gen_math_ops import mod
+# import tensorflow_datasets as tfds
+# from vpnn import VpnnLayer
 
-(ds_train, ds_test), ds_info = tfds.load(
-    'mnist',
-    split=['train', 'test'],
-    shuffle_files=True,
-    as_supervised=True,
-    with_info=True,
-)
-
-
-def normalize_img(image, label):
-    """Normalizes images: `uint8` -> `float32`."""
-    return tf.cast(image, tf.float32) / 255., label
+# (ds_train, ds_test), ds_info = tfds.load(
+#     'mnist',
+#     split=['train', 'test'],
+#     shuffle_files=True,
+#     as_supervised=True,
+#     with_info=True,
+# )
 
 
-ds_train = ds_train.map(
-    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-ds_train = ds_train.cache()
-ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
-ds_train = ds_train.batch(128)
-ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
+# def normalize_img(image, label):
+#     """Normalizes images: `uint8` -> `float32`."""
+#     return tf.cast(image, tf.float32) / 255., label
 
 
-ds_test = ds_test.map(
-    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
-ds_test = ds_test.batch(128)
-ds_test = ds_test.cache()
-ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+# ds_train = ds_train.map(
+#     normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+# ds_train = ds_train.cache()
+# ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+# ds_train = ds_train.batch(128)
+# ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
 
+
+# ds_test = ds_test.map(
+#     normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+# ds_test = ds_test.batch(128)
+# ds_test = ds_test.cache()
+# ds_test = ds_test.prefetch(tf.data.experimental.AUTOTUNE)
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+x_train = x_train.reshape(-1, 28*28) / 255
+x_test = x_test.reshape(-1, 28*28) / 255
+# y_train = tf.keras.utils.to_categorical(y_train)
+# y_test = tf.keras.utils.to_categorical(y_test)
 
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
+    # tf.keras.layers.Flatten(input_shape=(28, 28, 1)),
     # VpnnLayer(n_layers=3, n_rotations=10),
+    tf.keras.layers.Input((28*28,)),
     vpnn(n_layers=3, n_rotations=10),
     tf.keras.layers.Dense(10, activation='softmax')
 ])
@@ -49,7 +58,8 @@ model.compile(
 )
 
 model.fit(
-    ds_train,
+    x=x_train,
+    y=y_train,
     epochs=30,
-    validation_data=ds_test,
+    validation_data=(x_test, y_test)
 )
