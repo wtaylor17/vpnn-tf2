b0ac687bff06f254dc777513650d8a872eb53438
logan-anderson <landerson2@upei.ca>
2020-10-21 10:16:15 -0300

    code cleanup

--- vpnn/__init__.py
+++ vpnn/__init__.py
@@ -1,3 +1,2 @@
 from . import layers, models
-from .vpnn_layer import VpnnLayer
 from .models import vpnn
--- vpnn/models.py
+++ vpnn/models.py
@@ -1,12 +1,11 @@
 import tensorflow as tf
-import numpy as np
-from typing import List, Callable, Union
+from typing import Callable, Union
 
 from .layers import Rotation, Permutation, Diagonal, Bias, SVDDownsize
 from . import activations
 
 
-def vpnn(input_dim: int = 2,
+def vpnn(input_dim: int = None,
          n_layers: int = 1,
          n_rotations: int = 1,
          theta_initializer: str = 'uniform',
@@ -43,56 +42,37 @@ def vpnn(input_dim: int = 2,
     :return: a tf.keras.Model
     """
 
-    # if input_dim % 2 != 0:
-    #     raise ValueError('input dimension must be even')
-
-    # input_tensor = tf.keras.Input(shape=(None,))
     model = tf.keras.Sequential()
-    # current_output = input_tensor
+    # Add an input layer if they want one
+    if(input_dim):
+        model.add(tf.keras.layers.Input((input_dim,)))
 
     for k in range(n_layers):
         for j in range(n_rotations):
             if use_permutations:
-                # current_output = Permutation()(current_output)
                 model.add(Permutation())
-            # current_output = Rotation(theta_initializer=theta_initializer)(current_output)
             model.add(Rotation(theta_initializer=theta_initializer))
 
         if use_diagonals:
-            # current_output = Diagonal(t_initializer=t_initializer, function=diagonal_fn)(current_output)
             model.add(Diagonal(t_initializer=t_initializer, function=diagonal_fn))
 
         for j in range(n_rotations):
             if use_permutations:
-                # current_output = Permutation()(current_output)
                 model.add(Permutation())
-            # current_output = Rotation(theta_initializer=theta_initializer)(current_output)
             model.add(Rotation(theta_initializer=theta_initializer))
 
         if use_bias:
-            # current_output = Bias(
-            #     bias_initializer=bias_initializer)(current_output)
             model.add(Bias(bias_initializer=bias_initializer))
         if k != n_layers - 1:
-            # current_output = activations.get(hidden_activation,
-            #                                  trainable=trainable_M,
-            #                                  M_init=M_init,
-            #                                  M_initializer=M_initializer)(current_output)
             model.add(tf.keras.layers.Lambda(activations.get(hidden_activation,
                                                              trainable=trainable_M,
                                                              M_init=M_init,
                                                              M_initializer=M_initializer)))
 
     if output_dim:
-        # current_output = SVDDownsize(output_dim)(current_output)
         model.add(SVDDownsize(output_dim))
-    # current_output = activations.get(output_activation,
-    #                                  trainable=trainable_M,
-    #                                  M_init=M_init,
-    #                                  M_initializer=M_initializer)(current_output)
     model.add(tf.keras.layers.Lambda(activations.get(output_activation,
                                                      trainable=trainable_M,
                                                      M_init=M_init,
                                                      M_initializer=M_initializer)))
-    # return tf.keras.Model(input_tensor, current_output)
     return model
